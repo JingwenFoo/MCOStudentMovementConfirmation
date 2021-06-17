432b29a3d2bdc780c55570200ab59055e3be3731
ikarosli9 <68456151+ikarosli9@users.noreply.github.com>
2021-06-17 16:28:17 +0800

    history

--- app/src/main/java/com/example/mcostudentmovementconfirmation/StudentPage.java
+++ app/src/main/java/com/example/mcostudentmovementconfirmation/StudentPage.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class StudentPage extends AppCompatActivity {
-Button logout, profile, post,checkIn;
+Button logout, profile, post,checkIn,history;
 TextView textName, textID;
 DatabaseReference ref;
     @Override
@@ -30,6 +30,7 @@ DatabaseReference ref;
         textID = (TextView) findViewById(R.id.textViewID);
         post = (Button) findViewById(R.id.btnPost);
         checkIn=(Button)findViewById(R.id.btnCheckIn);
+        history=(Button)findViewById(R.id.btnHistory);
 
         textID.setText(String.valueOf(preferences.getDataStatus(this)));
 
@@ -58,6 +59,14 @@ DatabaseReference ref;
             @Override
             public void onClick(View v) {
                 Intent profile = new Intent(StudentPage.this, ScanPage.class);
+                startActivity(profile);
+            }
+        });
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profile = new Intent(StudentPage.this, HistoryPage.class);
                 startActivity(profile);
             }
         });
