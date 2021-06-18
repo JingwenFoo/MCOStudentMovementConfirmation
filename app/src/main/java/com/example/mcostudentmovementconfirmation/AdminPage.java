7f6d74e6ae9002bee5594db3f478d8d550e25e77
norsyera77 <68344219+norsyera77@users.noreply.github.com>
2021-06-19 00:11:20 +0800

    manage account

--- app/src/main/java/com/example/mcostudentmovementconfirmation/AdminPage.java
+++ app/src/main/java/com/example/mcostudentmovementconfirmation/AdminPage.java
@@ -1,23 +1,20 @@
 package com.example.mcostudentmovementconfirmation;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
---
+++ app/src/main/java/com/example/mcostudentmovementconfirmation/myadapter.java
@@ -0,0 +1,157 @@
+package com.example.mcostudentmovementconfirmation;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class myadapter extends FirebaseRecyclerAdapter<model2,myadapter.myviewholder>
+{
+    public myadapter(@NonNull FirebaseRecyclerOptions<model2> options)
+    {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull final myviewholder holder, final int position, @NonNull final model2 model)
+    {
+        holder.studentID.setText(model.getStudentID());
+        holder.name.setText(model.getName());
+        holder.email.setText(model.getEmail());
+        holder.phone.setText(model.getPhone());
+        holder.state.setText(model.getState());
+
+
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DialogPlus dialogPlus=DialogPlus.newDialog(holder.studentID.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.dialogcontent))
+                        .setExpanded(true,1100)
+                        .create();
+
+                View myview=dialogPlus.getHolderView();
+                final EditText studentID=myview.findViewById(R.id.ustudentID);
+                final EditText name=myview.findViewById(R.id.uname);
+                final EditText email=myview.findViewById(R.id.uemail);
+                final EditText phone=myview.findViewById(R.id.phonetext);
+                final EditText state=myview.findViewById(R.id.statetext);
+                Button submit=myview.findViewById(R.id.usubmit);
+
+
+                studentID.setText(model.getStudentID());
+                name.setText(model.getName());
+                email.setText(model.getEmail());
+                phone.setText(model.getPhone());
+                state.setText(model.getState());
+
+                dialogPlus.show();
+
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Map<String,Object> map=new HashMap<>();
+                        map.put("course",studentID.getText().toString());
+                        map.put("name",name.getText().toString());
+                        map.put("email",email.getText().toString());
+
+
+                        FirebaseDatabase.getInstance().getReference().child("students")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        dialogPlus.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+                    }
+                });
+
+
+            }
+        });
+
+
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(holder.studentID.getContext());
+                builder.setTitle("Delete Panel");
+                builder.setMessage("Delete...?");
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseDatabase.getInstance().getReference().child("students")
+                                .child(getRef(position).getKey()).removeValue();
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+    } // End of OnBindViewMethod
+
+    @NonNull
+    @Override
+    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerow,parent,false);
+        return new myviewholder(view);
+    }
+
+
+    class myviewholder extends RecyclerView.ViewHolder
+    {
+
+
+
+        ImageView edit,delete;
+        TextView studentID,name,email,phone,state;
+        public myviewholder(@NonNull View itemView)
+        {
+            super(itemView);
+            studentID=(TextView)itemView.findViewById(R.id.studentID);
+            name=(TextView)itemView.findViewById(R.id.nametext);
+            email=(TextView)itemView.findViewById(R.id.emailtext);
+            phone=(TextView)itemView.findViewById(R.id.phonetext);
+            state=(TextView)itemView.findViewById(R.id.statetext);
+
+            edit=(ImageView)itemView.findViewById(R.id.editicon);
+            delete=(ImageView)itemView.findViewById(R.id.deleteicon);
+        }
+    }
+}
